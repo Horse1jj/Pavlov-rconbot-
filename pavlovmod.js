08eba07a7693704c676ec6ca8033a3d1d2a1a9f0
Horse1jj <foste1jj.seth@gmail.com>
2024-10-17 18:50:14 -0400

    Update pavlovmod.js

--- pavlovmod.js
+++ pavlovmod.js
@@ -1,4 +1,5 @@
-const rcon = require('pavlov-rcon'); // Import a Pavlov RCON library
+const rcon = require('pavlov-rcon');
+const Discord = require('discord.js');
 
 module.exports = {
     handleCommand(command, args, message, servers) {
@@ -6,37 +7,77 @@ module.exports = {
         const server = servers[serverName];
 
         if (!server) {
-            return message.reply('Server not found!');
+            const serverNotFoundEmbed = new Discord.MessageEmbed()
+                .setColor('#FF0000')
+                .setTitle('Server Not Found')
+                .setDescription(`Server "${serverName}" not found!`)
+                .setTimestamp();
+
+            return message.channel.send({ embeds: [serverNotFoundEmbed] });
         }
 
         switch (command) {
             case 'ban':
                 rcon.sendCommand(server, `Ban ${args[0]}`);
-                message.reply(`${args[0]} has been banned from ${serverName}.`);
+                const banEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Ban Successful')
+                    .setDescription(`${args[0]} has been banned from ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [banEmbed] });
                 break;
             case 'kick':
                 rcon.sendCommand(server, `Kick ${args[0]}`);
-                message.reply(`${args[0]} has been kicked from ${serverName}.`);
+                const kickEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Kick Successful')
+                    .setDescription(`${args[0]} has been kicked from ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [kickEmbed] });
                 break;
             case 'flush':
-                rcon.sendCommand(server, 'Flush');
-                message.reply(`Server ${serverName} has been flushed.`);
+                rcon.sendCommand(server, `Flush`);
+                const flushEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Flush Successful')
+                    .setDescription(`Server ${serverName} has been flushed.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [flushEmbed] });
                 break;
             case 'kill':
                 rcon.sendCommand(server, `Kill ${args[0]}`);
-                message.reply(`${args[0]} has been killed on ${serverName}.`);
+                const killEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Kill Successful')
+                    .setDescription(`${args[0]} has been killed in ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [killEmbed] });
                 break;
             case 'slap':
                 rcon.sendCommand(server, `Slap ${args[0]}`);
-                message.reply(`${args[0]} has been slapped on ${serverName}.`);
+                const slapEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Slap Successful')
+                    .setDescription(`${args[0]} has been slapped in ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [slapEmbed] });
                 break;
             case 'unban':
                 rcon.sendCommand(server, `Unban ${args[0]}`);
-                message.reply(`${args[0]} has been unbanned from ${serverName}.`);
+                const unbanEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Unban Successful')
+                    .setDescription(`${args[0]} has been unbanned from ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [unbanEmbed] });
                 break;
             default:
-                message.reply('Unknown mod command.');
-                break;
+                const unknownEmbed = new Discord.MessageEmbed()
+                    .setColor('#FF0000')
+                    .setTitle('Unknown Command')
+                    .setDescription(`The command "${command}" is not recognized.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [unknownEmbed] });
         }
     }
 };
