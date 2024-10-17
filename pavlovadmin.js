2b46108ce038753a314a3bad599d08378d52c176
Horse1jj <foste1jj.seth@gmail.com>
2024-10-17 18:49:44 -0400

    Update pavlovadmin.js

--- pavlovadmin.js
+++ pavlovadmin.js
@@ -1,4 +1,5 @@
 const rcon = require('pavlov-rcon');
+const Discord = require('discord.js');
 
 module.exports = {
     handleCommand(command, args, message, servers) {
@@ -6,41 +7,86 @@ module.exports = {
         const server = servers[serverName];
 
         if (!server) {
-            return message.reply('Server not found!');
+            const serverNotFoundEmbed = new Discord.MessageEmbed()
+                .setColor('#FF0000')
+                .setTitle('Server Not Found')
+                .setDescription(`Server "${serverName}" not found!`)
+                .setTimestamp();
+
+            return message.channel.send({ embeds: [serverNotFoundEmbed] });
         }
 
         switch (command) {
             case 'giveitem':
                 rcon.sendCommand(server, `GiveItem ${args[0]} ${args[1]}`);
-                message.reply(`${args[0]} has been given ${args[1]} on ${serverName}.`);
+                const giveItemEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Item Given')
+                    .setDescription(`${args[0]} has been given ${args[1]} on ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [giveItemEmbed] });
                 break;
             case 'givevehicle':
                 rcon.sendCommand(server, `GiveVehicle ${args[0]} ${args[1]}`);
-                message.reply(`${args[0]} has been given vehicle ${args[1]} on ${serverName}.`);
+                const giveVehicleEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Vehicle Given')
+                    .setDescription(`${args[0]} has been given vehicle ${args[1]} on ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [giveVehicleEmbed] });
                 break;
             case 'ignite':
                 rcon.sendCommand(server, `Ignite ${args[0]}`);
-                message.reply(`${args[0]} has been ignited on ${serverName}.`);
+                const igniteEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Ignite Successful')
+                    .setDescription(`${args[0]} has been ignited in ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [igniteEmbed] });
                 break;
             case 'godmode':
                 rcon.sendCommand(server, `GodMode ${args[0]}`);
-                message.reply(`${args[0]} is now in god mode on ${serverName}.`);
+                const godmodeEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('GodMode Enabled')
+                    .setDescription(`${args[0]} now has GodMode in ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [godmodeEmbed] });
                 break;
             case 'detonate':
                 rcon.sendCommand(server, `Detonate ${args[0]}`);
-                message.reply(`${args[0]} has been detonated on ${serverName}.`);
+                const detonateEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Detonation Successful')
+                    .setDescription(`${args[0]} has been detonated in ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [detonateEmbed] });
                 break;
             case 'rotate':
                 rcon.sendCommand(server, `RotateMap`);
-                message.reply(`Map rotated on ${serverName}.`);
+                const rotateEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Map Rotated')
+                    .setDescription(`Map has been rotated on ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [rotateEmbed] });
                 break;
             case 'switchmap':
                 rcon.sendCommand(server, `SwitchMap ${args[0]}`);
-                message.reply(`Map switched to ${args[0]} on ${serverName}.`);
+                const switchMapEmbed = new Discord.MessageEmbed()
+                    .setColor('#00FF00')
+                    .setTitle('Map Switched')
+                    .setDescription(`Map switched to ${args[0]} on ${serverName}.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [switchMapEmbed] });
                 break;
             default:
-                message.reply('Unknown admin command.');
-                break;
+                const unknownEmbed = new Discord.MessageEmbed()
+                    .setColor('#FF0000')
+                    .setTitle('Unknown Command')
+                    .setDescription(`The command "${command}" is not recognized.`)
+                    .setTimestamp();
+                message.channel.send({ embeds: [unknownEmbed] });
         }
     }
 };
